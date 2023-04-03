60650bbe055fda7839c20f7bc57335075348fc10
jack mossbarger <jack-mossbarger@uiowa.edu>
2023-04-03 16:10:08 -0500

    updated flaskhw

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, flash
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "your_random_string"
 
 friends_dict = [
    
@@ -51,6 +53,9 @@ def add():
             friend_dict
         )  # append this dictionary entry to the larger friends dictionary
         print(friends_dict)
+
+        flash('Record successfully added.', 'success')
+
         return redirect(url_for("index"))
     else:
         return redirect(url_for("index"))
@@ -60,6 +65,13 @@ def add():
 def about():
     return render_template("about.html", pageTitle="About my Web form",)
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template("500.html"), 500
 
 
 if __name__ == "__main__":
